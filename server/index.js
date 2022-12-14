8500167b4d62a4c74dcfff8ab7b09f6103843d67
samaksh mittal <sam.mittal44@gmail.com>
2022-12-15 01:57:38 +0530

    chore: connected application BE to MongoDb

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 // Configurations
 dotenv.config();
@@ -22,3 +26,13 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+// Mongoose Setup
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`Server started running at ${PORT}`))
+  .catch((error) => console.log(`Error Encountered: ${error}`));
