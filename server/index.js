705e927ca08de8ead11611f96208d70308a5d058
samaksh mittal <sam.mittal44@gmail.com>
2023-03-01 00:33:38 +0530

    feat: add getPrudctsWithStats query

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import salesRoutes from "./routes/sales.js";
 
 // data imports
 import User from "./models/User.js";
-import { dataUser } from "./data/index.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
 
 // Configurations
 dotenv.config();
@@ -43,5 +45,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server started running at ${PORT}`));
     //Injected Data once, dont inject again
     // User.insertMany(dataUser);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
   })
   .catch((error) => console.log(`Error Encountered: ${error}`));
