f6d754aa863be0174e34a721780694c4447015ca
samaksh mittal <sam.mittal44@gmail.com>
2023-03-01 14:41:00 +0530

    feat: fetch data from transactions API and disaply in table

--- client/src/scenes/transactions/transactions.js
+++ client/src/scenes/transactions/transactions.js
@@ -20,7 +20,63 @@ const Transactions = () => {
     sort: JSON.stringify(sort),
   });
 
-  return <div>Transactions</div>;
+  const columns = [
+    { field: "_id", headerName: "ID", flex: 1 },
+    { field: "userId", headerName: "User ID", flex: 1 },
+    { field: "createdAt", headerName: "Created At", flex: 1 },
+    {
+      field: "products",
+      headerName: "Number of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
+  return (
+    <Box m={"1.5rem 2.5rem"}>
+      <Header title={"TRANSACTIONS"} subtitle={"Entire List Of Transactions"} />
+      <Box
+        height={"80vh"}
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.transactions) || []}
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Transactions;
