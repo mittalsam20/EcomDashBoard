e454702adaebe7eb4195946382ae3864a8712e4b
samaksh mittal <sam.mittal44@gmail.com>
2023-03-01 11:08:21 +0530

    feat: product page development done

--- client/src/scenes/products/products.js
+++ client/src/scenes/products/products.js
@@ -20,7 +20,56 @@ const Product = (props) => {
   const theme = useTheme();
   const [isExpanded, setIsExpanded] = useState(false);
 
-  return <></>;
+  return (
+    <Card
+      sx={{
+        backgroundImage: "none",
+        borderRadius: "0.55rem",
+        backgroundColor: theme.palette.background.alt,
+      }}
+    >
+      <CardContent>
+        <Typography
+          sx={{ fontSize: 14 }}
+          color={theme.palette.secondary[700]}
+          gutterBottom
+        >
+          {category}
+        </Typography>
+        <Typography variant="5" component="div">
+          {name}
+        </Typography>
+        <Typography sx={{ mb: "1.5rem" }} color={theme.palette.secondary[400]}>
+          {`$${Number(price).toFixed(2)}`}
+        </Typography>
+        <Rating value={rating} readOnly />
+        <Typography variant="body2">{description}</Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          variant={"primary"}
+          size={"small"}
+          onClick={() => setIsExpanded(!isExpanded)}
+        >
+          {"See More"}
+        </Button>
+      </CardActions>
+      <Collapse
+        in={isExpanded}
+        timeout={"auto"}
+        unmountOnExit
+        sx={{ color: theme.palette.neutral[300] }}
+      />
+      <CardContent>
+        <Typography>id: {_id}</Typography>
+        <Typography>Supply Left: {supply}</Typography>
+        <Typography>Yearly Sales This year: {stat.yearlySalesTotal}</Typography>
+        <Typography>
+          Yearly Units Sold This Year: {stat.yearlyTotalSoldUnits}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 const Products = () => {
@@ -39,7 +88,31 @@ const Products = () => {
           sx={{ "& > div": { gridColumn: isNonMobile ? undefined : "span 4" } }}
           gridTemplateColumns={"repeat(4, minmax(0, 1fr))"}
         >
-          {data.map((product) => {})}
+          {data.map(
+            ({
+              _id,
+              name,
+              description,
+              price,
+              supply,
+              rating,
+              category,
+              stat,
+            }) => {
+              return (
+                <Product
+                  _id={_id}
+                  name={name}
+                  description={description}
+                  price={price}
+                  supply={supply}
+                  rating={rating}
+                  category={category}
+                  stat={stat}
+                />
+              );
+            }
+          )}
         </Box>
       ) : (
         <Box>{"LOADING...."}</Box>
