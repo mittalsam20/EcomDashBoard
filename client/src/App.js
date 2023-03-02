24b829253cb1b1c089bb69e1e1054ef8a859983e
samaksh mittal <sam.mittal44@gmail.com>
2023-03-03 00:22:51 +0530

    feat: add route and UI for monthly sales chart page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Transactions from "scenes/transactions/transactions";
 import Geography from "scenes/geography/geography";
 import Overview from "scenes/overview/overview";
 import Daily from "scenes/daily/daily";
+import Monthly from "scenes/monthly/monthly";
+
 const App = () => {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -30,6 +32,7 @@ const App = () => {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
