f44b585786930bf49b436e8b8944e2c754ddc1ed
samaksh mittal <sam.mittal44@gmail.com>
2023-03-03 13:24:18 +0530

    chore: prod trial

--- client/src/scenes/customers/customers.js
+++ client/src/scenes/customers/customers.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, useTheme } from "@mui/material";
-import Header from "components/Header";
+import Header from "../../components/Header";
 import { DataGrid } from "@mui/x-data-grid";
 import { useGetCustomersQuery } from "../../state/api";
 
