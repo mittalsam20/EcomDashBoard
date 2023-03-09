960d3861ad5151849a9ec8a41bb003f77dc363b2
samaksh mittal <sam.mittal44@gmail.com>
2023-03-09 13:00:34 +0530

    feat: add BE route and controller for dashBoard page data

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,10 +1,51 @@
 import User from "../models/User.js";
+import OverallStat from "../models/OverallStat.js";
+import Transaction from "../models/Transaction.js";
 
 export const getUser = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
     res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getDashboardStats = async (req, res) => {
+  try {
+    //Hardcoded Values
+    const currentMonth = "November";
+    const currentYear = 2021;
+    const currentDay = "2021-11-15";
+
+    const transactions = await Transaction.find()
+      .limit(50)
+      .sort({ createdOn: -1 });
+    const overAllStat = await OverallStat.find({ year: currentYear });
+    const {
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      dailyData,
+      salesByCategory,
+    } = overAllStat[0];
+
+    const thisMonthStats = monthlyData.find({ month: currentMonth });
+    const todayStats = dailyData.find({ date: currentDay });
+
+    res.status(200).json({
+      totalCustomers,
+      yearlyTotalSoldUnits,
+      yearlySalesTotal,
+      monthlyData,
+      dailyData,
+      salesByCategory,
+      thisMonthStats,
+      todayStats,
+      transactions,
+    });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
